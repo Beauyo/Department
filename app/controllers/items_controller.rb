9e109aa9d09a76d4f52dab1e01086dba36564c59
Beau Hicken <Turbodiesel2500dee@hotmail.com>
2019-02-20 20:50:30 -0700

    almost done

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,56 @@
+class ItemsController < ApplicationController
+  def index
+    @items = @department.items
+  end
+
+  def show
+  end
+
+  def new
+    @item = @department.items.new
+    render partial: "form"
+  end
+
+  def create
+    @item = Department.items.new(topics_params)
+
+    if @item.save
+      redirect_to department_item_path(@department, @item)
+    else
+      render partial: "form"
+    end
+
+    def edit
+      render partial: "form"
+    end
+
+    def update
+      if @department.update(item_params)
+        redirect_to [@department, @item]
+      else
+        render edit:
+      end
+    end
+
+    def destroy
+     @item.destroy
+     redirect_to department_items_path(@department)
+
+    end
+
+    private
+    def set_department
+      @department = Department.find(params[:department_id])
+    end
+
+    def set_item
+      @item = Item.new(params[:id])
+    end
+
+    def item_params
+      params.require(:topic).permit(:name, :body, :department_id)
+    end
+
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_203355) do
+ActiveRecord::Schema.define(version: 2019_02_20_234156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "departments", force: :cascade do |t|
-    t.string "Item"
+    t.string "name"
+    t.string "address"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
